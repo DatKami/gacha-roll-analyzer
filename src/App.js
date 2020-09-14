e56e94daf62dd8400151c315da4e715636c8768a
DatKami <studiowolf8@gmail.com>
2020-09-13 21:47:54 -0400

    add chart

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Line } from 'react-chartjs-2';
+import ProbabilityDataTransform from './ProbabilityDataTransform';
 
 function App() {
+  const data = {
+      datasets: [{
+        data: ProbabilityDataTransform({probability: .03})
+      }]
+    },
+    options = {
+      scales: {
+        xAxes: [{
+          type: 'linear',
+          position: 'bottom'
+        }]
+      }
+    };
+
   return (
     <div className="App">
       <header className="App-header">
@@ -18,6 +34,10 @@ function App() {
         >
           Learn React
         </a>
+        <Line 
+          data={data} 
+          options={options}
+        />
       </header>
     </div>
   );

afcf5cab1cf95f5972786e7d0fa6b091332ea913
Brandon Ha <studiowolf8@gmail.com>
2020-10-15 19:47:02 -0400

    gh-pages

--- src/App.js
+++ src/App.js
@@ -56,18 +56,6 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
         <TextField id="probability" label="Probability (%)" value={probability} onChange={e => setProbability(e.target.value)}/>
         <FormControlLabel
           control={
